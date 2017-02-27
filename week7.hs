1dbd3af2cf2aaba18d81a1d89b1eb029c03c5295
Imre Gulyás <imre.gulyas@balabit.com>
2017-02-27 17:32:05 +0100

    szakkor

--- week7.hs
+++ week7.hs
@@ -4,7 +4,7 @@ streamToList :: Stream a -> [a]
 streamToList (Cons head stream) = head : streamToList stream
 
 instance Show a => Show (Stream a) where
-  show stream =  let
+    show stream =  let
       streamStr = init $ tail $ show $ take 20 $ streamToList stream
       in
       "<" ++ streamStr ++ ", ... >"
@@ -23,17 +23,62 @@ streamIterate rule head = let
         Cons head (restOfStream rule head)
 
 streamInterleave :: Stream a -> Stream a -> Stream a
-streamInterleave (Cons x xs) (Cons y ys) = Cons x $ Cons y $ streamInterleave xs ys
+streamInterleave (Cons x xs) zs = Cons x $ streamInterleave zs xs
 
 nats :: Stream Integer
 nats = streamIterate (+ 1) 0
 
---  the nth element in the stream (assuming the first element corresponds to n=1)
---  is the largest power of 2 which evenly divides n
-ruler :: Stream Integer
-ruler = let
-  powers = map (2^) [0 ..]
-  toRuler :: Integer -> Integer
-  toRuler n = last $ filter (\m -> mod n m == 0) $ filter (n >=) powers
-  in
-  streamMap toRuler $ streamIterate (+ 1) 1
+data Supply s a = S (Stream s -> (a, Stream s))
+
+get :: Supply s s
+get = S $ \(Cons elem (stream)) -> (elem, stream)
+
+pureSupply :: a -> Supply s a
+pureSupply a = S $ \stream -> (a, stream)
+
+mapSupply :: (a -> b) -> Supply s a -> Supply s b
+mapSupply f (S t) = S $ (\(first, second) -> (f first, second)) . t
+
+--mapSupply f (S t) = go
+--  where go xs = let (a, xs') = t xs
+--                in ((f a), xs')
+
+mapSupply2 :: (a -> b -> c) -> Supply s a -> Supply s b -> Supply s c
+-- mapSupply2 f (S t1) (S t2) = S $ (\(first, second) -> (f first, second)) . t
+mapSupply2 f (S t1) (S t2) = S go
+        where
+              go xs = let (a, xs')  = t1 xs
+                          (b, xs'') = t2 xs'
+                       in (f a b, xs'')
+
+bindSupply :: Supply s a -> (a -> Supply s b) -> Supply s b
+bindSupply (S t) f = S $ go
+  where
+    go s = let (a', s') = t s
+               (S g) = f a'
+            in  g s'
+
+runSupply :: Stream s -> Supply s a -> a
+runSupply s (S f) = fst $ f s
+
+instance Functor (Supply s) where
+    fmap = mapSupply
+
+instance Applicative (Supply s) where
+    pure = pureSupply
+    (<*>) = mapSupply2 id
+
+instance Monad (Supply s) where
+    return = pureSupply
+    (>>=) = bindSupply
+
+data Tree a = Node (Tree a) (Tree a) | Leaf a deriving Show
+
+labelTree :: Tree a -> Tree Integer
+labelTree t = runSupply nats (go t)
+  where
+    go :: Tree a -> Supply s (Tree s)
+    go node@(Node left right) = go left >>=
+
+
+    go (Leaf l) = pureSupply l
